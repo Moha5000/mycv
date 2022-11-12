8572a6a540ae700244290638fe5c9b74c4ee90f6
Mohammed Alhaifi <mohammed772985000@gmail.com>
2022-11-12 21:45:04 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Mohammed Alhaifi",
+  title: "Mechatronics Engineer & Computer Developer & Web Designer & Content Creator",
+  email: "Mohammed772985000@gmail.com",
+  gitHub: "Moha5000",
+  instagram: "mohammed_alhaifi",
+  linkedIn: "mohammed-alhaifi-057764235",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
